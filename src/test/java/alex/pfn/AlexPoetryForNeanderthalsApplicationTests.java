7b5ab2c32a7aa0171297d9414ff1b584fbcdcb8a
alexshields34 <alexshields34@gmail.com>
2024-06-14 14:01:51 -0400

    Add files via upload

---
+++ src/test/java/alex/pfn/AlexPoetryForNeanderthalsApplicationTests.java
@@ -0,0 +1,13 @@
+package alex.pfn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlexPoetryForNeanderthalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
